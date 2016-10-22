7b66930c1febe702541367575aba2321f97f1083
AlexNPavel <alexpavel123@gmail.com>
2016-10-22 13:27:52 -0400

    Fix index.dart for new directory structure

--- web/index.dart
+++ web/index.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, AlexNPavel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:SMSChorinator/main_app.dart';
+import 'package:SMSChorinator/client/main_app.dart';
 import 'package:polymer/polymer.dart';
 
 /// [MainApp] used!
