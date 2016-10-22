52e5c76f29ed8575a64656dbe9522dd0e2305721
AlexNPavel <alexpavel123@gmail.com>
2016-10-22 13:28:24 -0400

    Add some server source and add rpc package

---
+++ lib/server/chorinator_api.dart
@@ -0,0 +1,18 @@
+library chorinator.server;
+
+import 'package:rpc/rpc.dart';
+
+// This class defines the interface that the server provides.
+@ApiClass(version: 'v1')
+class ChorinatorApi {
+
+/*
+  @ApiMethod(method: 'POST', path: 'add')
+
+  @ApiMethod(method: 'DELETE', path: 'remove')
+
+  //Default is GET, method unneccessary
+  @ApiMethod(method: 'GET', path: 'chores')
+*/
+
+}
