0718c6915fc103b529e43738fdefc2336506de0d
AlexNPavel <alexpavel123@gmail.com>
2016-11-14 22:06:22 -0500

    Use Angular2 instead of PolymerDart

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,10 @@
 // Copyright (c) 2016, AlexNPavel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:SMSChorinator/client/main_app.dart';
-import 'package:polymer/polymer.dart';
+import 'package:angular2/platform/browser.dart';
 
-/// [MainApp] used!
-main() async {
-  await initPolymer();
+import 'package:SMSChorinator/client/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
 }
