7031bfb287f1c7942d3392370cd868911a1bcf4e
AlexNPavel <alexpavel123@gmail.com>
2016-12-30 16:45:55 -0500

    Remove api prefix

--- bin/server.dart
+++ bin/server.dart
@@ -5,9 +5,8 @@ import 'package:rpc/rpc.dart';
 import 'package:SMSChorinator/server/db_handler.dart';
 import 'package:SMSChorinator/server/chorinator_api.dart';
 
-const String _API_PREFIX = '/api';
 final ApiServer _apiServer =
-    new ApiServer(apiPrefix: _API_PREFIX, prettyPrint: true);
+    new ApiServer(prettyPrint: true);
 
 class MainServer {
   static final DbHandler db = new DbHandler();
